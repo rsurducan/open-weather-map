a22a39d80d9e119d69a94b948afb10f4c2ada737
Radu Surducan <radu.surducan@autodesk.com>
2015-11-02 20:48:40 -0500

    I have a working version

---
+++ owm-app.js
@@ -0,0 +1,30 @@
+angular.module('OWMApp', ['ngRoute'])
+    .config(['$routeProvider', function($routeProvider){
+        $routeProvider.when('/', {
+            templateUrl : 'home.html',
+            controller : 'HomeCtrl'
+        }).when('/cities/:city', {
+        	templateUrl : 'city.html',
+        	controller : 'CityCtrl',
+        	resolve : {
+        		city: function(owmCities, $route, $location) {
+        			var city = $route.current.params.city;
+        			city = city.replace('_', ' ');
+        			if (owmCities.indexOf(city) == -1) {
+        				$location.path('/error');
+        				return;
+        			}
+        			return city;
+        		}
+        	}
+        }).when('/error', {
+        	template : '<p>Error - Page Not Found</p>'
+        }).otherwise('/error');
+    }])
+    .controller('HomeCtrl', function($scope) {
+        //empty for now
+    })
+    .controller('CityCtrl', function($scope, city) {	
+    	$scope.city = city;
+    })
+    .value('owmCities', ['New York', 'Dallas', 'Chicago']);
